e8df1795a3f3f2a855b56c40926341e787c46dac
shoaib shaikh <shoaib.shaikh@lemosys.com>
2023-07-03 18:30:25 +0530

    d

--- controllers/messages.js
+++ controllers/messages.js
@@ -257,7 +257,7 @@ exports.getSpiltChats = async (req, res) => {
     sql1 =
       "SELECT users.name, billing_group.group_id,billing_group.spliting_amount,( SELECT GROUP_CONCAT(users.profile_picture) FROM users LEFT JOIN billing_group_users ON billing_group_users.user_id=users.id WHERE billing_group_users.group_id=billing_group.group_id AND billing_group_users.user_id!=" +
       req.query.login_user_id +
-      "  ) AS group_users_image,(select sum( case when billing_group_users.payment_amount IS NOT NULL then billing_group_users.payment_amount else 0 end )/COUNT(*)  from `billing_group_users` WHERE billing_group_users.group_id=billing_group.group_id ) AS percentage,(select  billing_group.spliting_amount/COUNT(*)  from `billing_group_users` WHERE billing_group_users.group_id=billing_group.group_id ) AS each_split ,  FROM billing_group  LEFT JOIN billing_group_users ON billing_group_users.group_id=billing_group.group_id  LEFT JOIN users ON users.id=billing_group_users.group_id     WHERE billing_group_users.group_id=" +
+      "  ) AS group_users_image,(select sum( case when billing_group_users.payment_amount IS NOT NULL then billing_group_users.payment_amount else 0 end )/COUNT(*)  from `billing_group_users` WHERE billing_group_users.group_id=billing_group.group_id ) AS percentage,(select  billing_group.spliting_amount/COUNT(*)  from `billing_group_users` WHERE billing_group_users.group_id=billing_group.group_id ) AS each_split   FROM billing_group  LEFT JOIN billing_group_users ON billing_group_users.group_id=billing_group.group_id  LEFT JOIN users ON users.id=billing_group_users.group_id     WHERE billing_group_users.group_id=" +
       req.query.group_id +" GROUP BY users.id" ;
 
     console.log("ssq1========", sql1);
