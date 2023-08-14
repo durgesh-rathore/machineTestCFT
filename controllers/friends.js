8c445b542588f63481bb3c6016b146e2e9cccff7
shoaib shaikh <shoaib.shaikh@lemosys.com>
2023-08-14 19:06:00 +0530

    d

--- controllers/friends.js
+++ controllers/friends.js
@@ -349,8 +349,7 @@ exports.friendsList = function (req, res) {
       var sql =
         "SELECT " +
         a +
-        ",users.name,users_requests.*,users.id, (SELECT  COUNT(users_requests.request_for) FROM users_requests WHERE users_requests.is_follow!=0  AND users_requests.request_for=users.id ) AS followed_by FROM users LEFT JOIN users_requests ON users_requests.request_for=users.id WHERE  users.is_group=0 AND (users_requests.is_both_follow=0 OR users_requests.is_both_follow IS null  )  AND users.id <>'" +
-        req.query.login_user_id +
+        ",users.name,users_requests.*,users.id, (SELECT  COUNT(users_requests.request_for) FROM users_requests WHERE users_requests.is_follow!=0  AND users_requests.request_for=users.id ) AS followed_by FROM users LEFT JOIN users_requests ON users_requests.request_for=users.id  LEFT JOIN users_requests UR ON UR.user_id=users.id       WHERE users.is_group=0     AND (users_requests.is_both_follow=0 OR (users_requests.is_both_follow IS null AND (UR.is_both_follow IS NULL OR UR.is_both_follow=0) ) )  AND users.id <>'" +        req.query.login_user_id +
         "'  AND ( users_requests.request_for <>'" +
         req.query.login_user_id +
         "' OR users_requests.request_for IS NULL ) AND (users_requests.is_accepted=0 OR  users_requests.is_accepted IS NULL) AND (users_requests.is_reject=0 OR users_requests.is_reject IS NULL ) AND (users_requests.is_request=0 OR users_requests.is_request IS NULL ) AND ((users_requests.is_follow=0 OR users_requests.is_follow IS NULL )  OR users_requests.user_id<> '" +
@@ -367,8 +366,7 @@ exports.friendsList = function (req, res) {
     // "' OR  users_requests.user_id IS NULL)
     connection.query(sql, async function (err, users) {
       var sqlCount1 =
-        "SELECT  COUNT(users.id) AS total_count FROM users LEFT JOIN users_requests ON users_requests.request_for=users.id WHERE users.is_group=0  AND (users_requests.is_both_follow=0 OR users_requests.is_both_follow IS null  )  AND users.id <>'" +
-        req.query.login_user_id +
+        "SELECT  COUNT(users.id) AS total_count FROM users LEFT JOIN users_requests ON users_requests.request_for=users.id WHERE users.is_group=0         AND (users_requests.is_both_follow=0 OR (users_requests.is_both_follow IS null AND (UR.is_both_follow IS NULL OR UR.is_both_follow=0) ) ) AND users.id <>'" +        req.query.login_user_id +
         "'  AND ( users_requests.request_for <>'" +
         req.query.login_user_id +
         "' OR users_requests.request_for IS NULL ) AND (users_requests.is_accepted=0 OR  users_requests.is_accepted IS NULL) AND (users_requests.is_reject=0 OR users_requests.is_reject IS NULL ) AND (users_requests.is_request=0 OR users_requests.is_request IS NULL ) AND (users_requests.is_follow=0 OR users_requests.is_follow IS NULL )  " +
