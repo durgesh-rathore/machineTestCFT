c7f6ba58d377cfda74a5fc1663383089a91c67a8
shoaib shaikh <shoaib.shaikh@lemosys.com>
2023-08-28 19:02:54 +0530

    d

--- controllers/posts.js
+++ controllers/posts.js
@@ -288,7 +288,7 @@ exports.getPostsAndEventsList = function (req, res) {
     req.query.login_user_id +
     " AND (users_requests.is_accepted=1 OR users_requests.is_follow_by_request_for=1 )   AND users_requests.is_reject=0  AND users_requests.is_block=0  )         ) )  OR (events.visibilitySelectUsers=2 AND visibility.user_id<>" +
     req.query.login_user_id +
-    " )  OR (events.visibilitySelectUsers=4 AND groups_users.user_id<>" +
+    " )  OR (events.visibilitySelectUsers=4 AND groups_users.user_id=" +
     req.query.login_user_id +
     ")     ";
 
