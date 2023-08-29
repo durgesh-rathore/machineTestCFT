3b102cddb5c42f59ce5d5ad710a4c19908035243
shoaib shaikh <shoaib.shaikh@lemosys.com>
2023-08-29 13:52:57 +0530

    d

--- controllers/friends.js
+++ controllers/friends.js
@@ -209,7 +209,7 @@ exports.requestForUser = function (req, res) {
                 usersRequest[0].user_id +
                 ",is_follow_by_request_for="+usersRequest[0].is_follow+", is_follow= "+usersRequest[0].is_follow_by_request_for+"  WHERE id= " +
                 usersRequest[0].id;
-                
+
               pushNotification(
                 notificationFor[0].divice_token,
                  "Frieds request for you by "+(req.body.login_user_name?req.body.login_user_name:'')+"",
@@ -476,7 +476,7 @@ exports.friendsList = function (req, res) {
       var sql =
         "SELECT " +
         a +
-        ",users.name, users_requests.*,users.id,(SELECT  COUNT(users_requests.request_for) FROM users_requests WHERE users_requests.is_follow!=0  AND users_requests.request_for=users.id ) AS followed_by  FROM users_requests LEFT JOIN users ON users.id=users_requests.user_id WHERE users_requests.request_for='" +
+        ",users.name, users_requests.*,users.id,  (SELECT  COUNT(users_requests.request_for) FROM users_requests WHERE users_requests.is_follow!=0  AND users_requests.request_for=users.id ) AS followed_by  FROM users_requests LEFT JOIN users ON users.id=users_requests.user_id WHERE users_requests.request_for='" +
         req.query.login_user_id +
         "' AND  users_requests.is_request=1 AND users_requests.is_reject<>1 AND users_requests.is_block<>1 AND users_requests.is_accepted<>1 " +
         condition +
@@ -489,13 +489,13 @@ exports.friendsList = function (req, res) {
       var sql =
         "SELECT " +
         a +
-        ",users.name,users_requests.*,users.id, (SELECT  COUNT(users_requests.request_for) FROM users_requests WHERE users_requests.is_follow!=0  AND users_requests.request_for=users.id ) AS followed_by  FROM users_requests LEFT JOIN users ON (   users.id =  case when users_requests.user_id!=" +
+        ",users.name,users_requests.*,(CASE WHEN (users_requests.user_id="+req.query.login_user_id+") THEN users_requests.is_follow ELSE users_requests.is_follow_by_request_for END ) AS is_follow,users.id, (SELECT  COUNT(users_requests.request_for) FROM users_requests WHERE users_requests.is_follow!=0  AND users_requests.request_for=users.id ) AS followed_by  FROM users_requests LEFT JOIN users ON (   users.id =  case when users_requests.user_id<>" +
         req.query.login_user_id +
         " Then users_requests.user_id ELSE users_requests.request_for END)  WHERE  ( users_requests.user_id='" +
         req.query.login_user_id +
         " ' OR users_requests.request_for='" +
         req.query.login_user_id +
-        "' )  AND users_requests.is_reject=0 AND users_requests.is_block=0 AND (users_requests.is_accepted=1   OR ((users_requests.is_request=1 OR users_requests.is_follow=1) AND (users_requests.user_id ='" +
+        "' )  AND users_requests.is_reject=0 AND users_requests.is_block=0 AND (users_requests.is_accepted=1   OR ((users_requests.is_request=1 OR users_requests.is_follow=1 OR ) AND (users_requests.user_id ='" +
         req.query.login_user_id +
         " ' OR users_requests.is_both_follow=1 ))   )   " +
         condition +
@@ -508,7 +508,7 @@ exports.friendsList = function (req, res) {
       var sql =
         "SELECT " +
         a +
-        ",users.name,users_requests.*,users.id, (SELECT  COUNT(users_requests.request_for) FROM users_requests WHERE users_requests.is_follow!=0  AND users_requests.request_for=users.id ) AS followed_by FROM users LEFT JOIN users_requests ON ((users_requests.request_for=users.id AND users_requests.user_id=" +
+        ",users.name,users_requests.*,(CASE WHEN (users_requests.user_id="+req.query.login_user_id+") THEN users_requests.is_follow ELSE users_requests.is_follow_by_request_for END ) AS is_follow,users.id, (SELECT  COUNT(users_requests.request_for) FROM users_requests WHERE users_requests.is_follow!=0  AND users_requests.request_for=users.id ) AS followed_by FROM users LEFT JOIN users_requests ON ((users_requests.request_for=users.id AND users_requests.user_id=" +
         req.query.login_user_id +
         ") OR  (users_requests.user_id=users.id AND users_requests.request_for=" +
         req.query.login_user_id +
